9e4f7ee1f221efa421f53e4fb89f4d34bcc06b28
Shota_Abashidze <101672572+Showik8@users.noreply.github.com>
2025-02-17 22:48:07 +0400

    Created Quiz Router and Installed React

---
+++ API/API.js
@@ -0,0 +1,103 @@
+const javascript =[
+    {
+      "question": "What does 'NaN' stand for in JavaScript?",
+      "options": [
+        "Not a Number",
+        "Null and Nothing",
+        "Number and Null",
+        "None and Nothing"
+      ],
+      "correct_answer": "Not a Number"
+    },
+    {
+      "question": "Which of the following is used to define a constant in JavaScript?",
+      "options": [
+        "var",
+        "let",
+        "const",
+        "define"
+      ],
+      "correct_answer": "const"
+    },
+    {
+      "question": "What will be the output of the following code: `console.log(2 + '2')`?",
+      "options": [
+        "4",
+        "22",
+        "undefined",
+        "NaN"
+      ],
+      "correct_answer": "22"
+    },
+    {
+      "question": "Which method is used to remove the last element from an array in JavaScript?",
+      "options": [
+        "pop()",
+        "push()",
+        "shift()",
+        "unshift()"
+      ],
+      "correct_answer": "pop()"
+    },
+    {
+      "question": "Which of the following is a falsy value in JavaScript?",
+      "options": [
+        "0",
+        "false",
+        "undefined",
+        "all of the above"
+      ],
+      "correct_answer": "all of the above"
+    },
+    {
+      "question": "What is the correct way to write a function in JavaScript?",
+      "options": [
+        "function = myFunction() {}",
+        "function myFunction() {}",
+        "function: myFunction() {}",
+        "myFunction() => {}"
+      ],
+      "correct_answer": "function myFunction() {}"
+    },
+    {
+      "question": "Which of the following is NOT a primitive data type in JavaScript?",
+      "options": [
+        "String",
+        "Number",
+        "Object",
+        "Boolean"
+      ],
+      "correct_answer": "Object"
+    },
+    {
+      "question": "How do you create an object in JavaScript?",
+      "options": [
+        "var obj = {}",
+        "var obj = []",
+        "var obj = ()",
+        "var obj = <>"
+      ],
+      "correct_answer": "var obj = {}"
+    },
+    {
+      "question": "What does the 'this' keyword refer to in JavaScript?",
+      "options": [
+        "The current function",
+        "The current object",
+        "The window object",
+        "The global object"
+      ],
+      "correct_answer": "The current object"
+    },
+    {
+      "question": "How do you add a comment in JavaScript?",
+      "options": [
+        "// This is a comment",
+        "# This is a comment",
+        "/* This is a comment */",
+        "Both A and C"
+      ],
+      "correct_answer": "Both A and C"
+    }
+  ];
+
